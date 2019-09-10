7a1ff2f5a12f4875a2d06d01f3c1332720f95b29
Mario Adamo Jr <marioadamojr@gmail.com>
2019-09-10 22:52:02 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -8,4 +8,9 @@ class Admin::PreferencesController < ApplicationController
     @preference.update(preference_params)
     redirect_to admin_preferences_path
   end
+
+  private
+  def preference_params
+    params.require(:preference).permit(:allow_create_songs, :allow_create_artists, :song_sort_order, :artist_sort_order)
+  end
 end
