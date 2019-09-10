0caeae408ac47824518e442b28b0d61ef973e853
Mario Adamo Jr <marioadamojr@gmail.com>
2019-09-10 22:51:22 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,4 +1,5 @@
 class Admin::PreferencesController < ApplicationController
   def index
+    @preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true, song_sort_order: "DESC", artist_sort_order: "DESC")
   end
 end
