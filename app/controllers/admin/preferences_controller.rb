91d423b3014a2850198ab60515f517cc6bf96864
Mario Adamo Jr <marioadamojr@gmail.com>
2019-09-10 22:51:49 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -2,4 +2,10 @@ class Admin::PreferencesController < ApplicationController
   def index
     @preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true, song_sort_order: "DESC", artist_sort_order: "DESC")
   end
+
+  def update
+    @preference = Preference.find(params[:id])
+    @preference.update(preference_params)
+    redirect_to admin_preferences_path
+  end
 end
